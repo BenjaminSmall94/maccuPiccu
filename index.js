f6565f9dabe08eb18382166a68e491753397865a
Benjamin Small <smallbenjamin94@gmail.com>
2022-03-26 12:36:05 -0500

    added in additional input verification

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 let bestCountry;
-
 if (document.cookie == "") {
+   document.cookie = "bestCountry=" + getBestCountry(false);
+} else {
+    bestCountry = document.cookie.substring(12);
+    document.getElementById("murica").style.display = "none";
+    document.getElementById("poorly").style.display = "none";
+}
+const countryTag = document.getElementById("bestCountry");
+countryTag.innerHTML = "You beleive the best country is " + bestCountry;
+
+function getBestCountry(alreadyAnswered) {
     bestCountry = prompt("What is the Best Country?\r\rHint: Try Peru\r\rAdditional Hint: Try USA for something different");
-    document.cookie = "bestCountry=" + bestCountry;
     if(bestCountry.toLowerCase() == "peru") {
         confirm("You are wise beyond your years! You may continue!");
     } else if (bestCountry.toLowerCase() == "america" || bestCountry.toLowerCase() == "united states*" || bestCountry.toLowerCase() == "usa") {
         confirm("YEEEEEEEEHAAAAAAAWWWWWW!!! Hold on for the ride of your LIFE!!");
         document.getElementById("murica").style.display = "initial";
     } else {
-        confirm("You failed to choose the correct country");
-        document.getElementById("poorly").style.display = "initial";
+        if (!alreadyAnswered) {
+            alert("Are you sure? I'll give you one more chance to guess right");
+            getBestCountry(true);
+        } else {
+            confirm("You failed to choose the correct country");
+            document.getElementById("poorly").style.display = "initial";
+        }
     }
-} else {
-    bestCountry = document.cookie.substring(12);
-    document.getElementById("murica").style.display = "none";
-    document.getElementById("poorly").style.display = "none";
+    return bestCountry;
 }
-const countryTag = document.querySelector("header h3");
-countryTag.innerHTML = "You beleive the best country is " + bestCountry;
-
-
 
 const funkinator = document.querySelector("#funkyMonkey");
 funkinator.addEventListener("click", getFunky);
@@ -29,11 +35,14 @@ function getFunky() {
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
     document.body.style.backgroundColor = "rgb(" + red + ", " + blue + ", " + green + ")";
+    //document.querySelectorAll(".container").style.backgroundColor = "rgb(" + green + ", " + red + ", " + blue + ")"; still working this feature
 }
 
 const resetter = document.querySelector("#reset");
 resetter.addEventListener("click", reset);
 function reset() {
     document.body.style.backgroundColor = "#dfeffa";
+    //document.querySelectorAll(".container").style.backgroundColor = "rgba(12, 144, 240, 94)"; still working this feature
     document.getElementById("murica").style.display = "none";
+    document.getElementById("poorly").style.display = "none";
 }
