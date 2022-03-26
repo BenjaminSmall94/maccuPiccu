66ace16073a1d158d84d9e95b7ad4c8b8ec73d52
Benjamin Small <smallbenjamin94@gmail.com>
2022-03-26 03:05:11 -0500

    Changed popup functionality to internal gifs

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const funkinator = document.querySelector("#funkyMonkey");
-const resetter = document.querySelector("#reset");
 let bestCountry;
 
 if (document.cookie == "") {
-    bestCountry = prompt("What is the Best Country?\r\rHint: Try Peru\r\rAdditional Hint: Try USA for something different\r\rFinal Hint: Popup blocker is discouraged");
+    bestCountry = prompt("What is the Best Country?\r\rHint: Try Peru\r\rAdditional Hint: Try USA for something different");
     document.cookie = "bestCountry=" + bestCountry;
     if(bestCountry.toLowerCase() == "peru") {
         confirm("You are wise beyond your years! You may continue!");
     } else if (bestCountry.toLowerCase() == "america" || bestCountry.toLowerCase() == "united states*" || bestCountry.toLowerCase() == "usa") {
         confirm("YEEEEEEEEHAAAAAAAWWWWWW!!! Hold on for the ride of your LIFE!!");
-        window.open("https://tenor.com/view/happy4th-of-july-merica-usa-america-gif-12105505", '_blank');
+        document.getElementById("murica").style.display = "initial";
     } else {
         confirm("You failed to choose the correct country");
-        window.open("https://tenor.com/view/himym-ted-you-chose-poorly-you-have-choosen-poorly-elegir-gif-13625278", '_blank');
+        document.getElementById("poorly").style.display = "initial";
     }
 } else {
     bestCountry = document.cookie.substring(12);
+    document.getElementById("murica").style.display = "none";
+    document.getElementById("poorly").style.display = "none";
 }
 const countryTag = document.querySelector("header h3");
 countryTag.innerHTML = "You beleive the best country is " + bestCountry;
 
 
 
+const funkinator = document.querySelector("#funkyMonkey");
 funkinator.addEventListener("click", getFunky);
 function getFunky() {
     let red = Math.floor(Math.random() * 256);
@@ -30,7 +31,9 @@ function getFunky() {
     document.body.style.backgroundColor = "rgb(" + red + ", " + blue + ", " + green + ")";
 }
 
+const resetter = document.querySelector("#reset");
 resetter.addEventListener("click", reset);
 function reset() {
     document.body.style.backgroundColor = "#dfeffa";
+    document.getElementById("murica").style.display = "none";
 }
