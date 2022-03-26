6c488574177f3ea647bcc93975adc9c05dde1c4a
Benjamin Small <smallbenjamin94@gmail.com>
2022-03-26 17:19:33 -0500

    added page interactivity with loops according to lab 8

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 let bestCountry;
+let bestCountryIndex;
+let photoCount;
 if (document.cookie == "") {
-   document.cookie = "bestCountry=" + getBestCountry(false);
+    bestCountry = getBestCountry(false);
+    photoCount = getPhotoCount("How corresponding photos would you like to see at the bottom of the page");
+    document.cookie = "bestCountry=" + bestCountry;
+    document.cookie = "photoCount=" + photoCount;
+    document.cookie = "bestCountryIndex=" + bestCountryIndex;
 } else {
-    bestCountry = document.cookie.substring(12);
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieArray = decodedCookie.split(';');
+    bestCountry = cookieArray[0].substring(12);
+    photoCount = cookieArray[1].substring(12);
+    bestCountryIndex = cookieArray[2].substring(18);
     document.getElementById("murica").style.display = "none";
     document.getElementById("poorly").style.display = "none";
 }
+console.log(bestCountry + " " + photoCount + " " + bestCountryIndex);
 document.getElementById("bestCountry").innerHTML = "You beleive the best country is " + bestCountry;
+if(photoCount != 0) {
+    console.log("here I am");
+    let div = document.getElementById("favoriteCountry");
+    div.style.display = "block";
+    let h3 = document.createElement("h3");
+    div.append(bestCountry, h3);
+    for(let i = 0; i < photoCount; i++) {
+        console.log("I'm in the loop");
+        if(bestCountryIndex == 0) {
+            div.insertAdjacentHTML("beforeend", '<div class="portraits"> <h4>' + bestCountry + '</h4> <img src="images/PeruvianFlag.png" alt="Peruvian Flag"></a></div>');
+        } else if(bestCountryIndex == 1) {
+            div.insertAdjacentHTML("beforeend", '<div class="portraits"> <h4>Party Time!</h4> <img src="images/\'Murica.gif" alt="The Great American Experience"></a></div>');
+        } else {
+            div.insertAdjacentHTML("beforeend", '<div class="portraits"> <h4>' + bestCountry + ' is WRONG!</h4> <img src="images/poorly.gif" alt="' + bestCountry + ' is WRONG!"></a></div>');
+        }
+    }
+}
 
 function getBestCountry(alreadyAnswered) {
-    bestCountry = prompt("What is the Best Country?\r\rHint: Try Peru\r\rAdditional Hint: Try USA for something different");
-    if(bestCountry.toLowerCase() == "peru") {
+    let answer = prompt("What is the Best Country?\r\rHint: Try Peru\r\rAdditional Hint: Try USA for something different");
+    if(answer.toLowerCase() == "peru") {
         confirm("You are wise beyond your years! You may continue!");
-    } else if (bestCountry.toLowerCase() == "america" || bestCountry.toLowerCase() == "united states*" || bestCountry.toLowerCase() == "usa") {
+        bestCountryIndex = 0;
+    } else if (answer.toLowerCase() == "america" || answer.toLowerCase() == "united states*" || answer.toLowerCase() == "usa") {
         confirm("YEEEEEEEEHAAAAAAAWWWWWW!!! Hold on for the ride of your LIFE!!");
         document.getElementById("murica").style.display = "initial";
+        bestCountryIndex = 1;
     } else {
         if (!alreadyAnswered) {
             alert("Are you sure? I'll give you one more chance to guess right");
@@ -22,9 +52,21 @@ function getBestCountry(alreadyAnswered) {
         } else {
             confirm("You failed to choose the correct country");
             document.getElementById("poorly").style.display = "initial";
+            bestCountryIndex = 2;
         }
     }
-    return bestCountry;
+    return answer;
+}
+
+function getPhotoCount(Message) {
+    let answer = prompt(Message);
+    answer = parseInt(answer);
+    if(Number.isInteger(answer) && answer >= 0 && answer <= 3) {
+        return answer;
+    } else {
+        getPhotoCount("You answer must be a number between 0 and 3\r\rHow corresponding photos would you like to see at the bottom of the page"); 
+    }
+
 }
 
 const funkinator = document.querySelector("#funkyMonkey");
