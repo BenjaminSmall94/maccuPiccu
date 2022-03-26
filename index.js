ff3becbb4b0775ca8ec6da3a7f2f69fd6432da95
Benjamin Small <smallbenjamin94@gmail.com>
2022-03-26 00:00:34 -0500

    Added seesion cookies to stop recurring prompts

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const bestCountry = prompt("What is the Best Country?\r\rHint: Try Peru\r\rAdditional Hint: Try USA for something different\r\rFinal Hint: Popup blocker is discouraged");
 const funkinator = document.querySelector("#funkyMonkey");
 const resetter = document.querySelector("#reset");
+let bestCountry;
 
-if(bestCountry.toLowerCase() == "peru") {
-    confirm("You are wise beyond your years! You may continue!");
-} else if (bestCountry.toLowerCase() == "america" || bestCountry.toLowerCase() == "united states*" || bestCountry.toLowerCase() == "usa") {
-    confirm("YEEEEEEEEHAAAAAAAWWWWWW!!! Hold on for the ride of your LIFE!!");
-    window.open("https://tenor.com/view/happy4th-of-july-merica-usa-america-gif-12105505", '_blank');
+if (document.cookie == "") {
+    bestCountry = prompt("What is the Best Country?\r\rHint: Try Peru\r\rAdditional Hint: Try USA for something different\r\rFinal Hint: Popup blocker is discouraged");
+    document.cookie = "bestCountry=" + bestCountry;
+    if(bestCountry.toLowerCase() == "peru") {
+        confirm("You are wise beyond your years! You may continue!");
+    } else if (bestCountry.toLowerCase() == "america" || bestCountry.toLowerCase() == "united states*" || bestCountry.toLowerCase() == "usa") {
+        confirm("YEEEEEEEEHAAAAAAAWWWWWW!!! Hold on for the ride of your LIFE!!");
+        window.open("https://tenor.com/view/happy4th-of-july-merica-usa-america-gif-12105505", '_blank');
+    } else {
+        confirm("You failed to follow instructions");
+        window.open("https://tenor.com/view/himym-ted-you-chose-poorly-you-have-choosen-poorly-elegir-gif-13625278", '_blank');
+    }
 } else {
-    confirm("You failed to follow instructions");
-    window.open("https://tenor.com/view/himym-ted-you-chose-poorly-you-have-choosen-poorly-elegir-gif-13625278", '_blank');
+    bestCountry = document.cookie.substring(12);
 }
+const countryTag = document.querySelector("header h3");
+countryTag.innerHTML = "You beleive the best country is " + bestCountry;
+
+
 
 funkinator.addEventListener("click", getFunky);
 function getFunky() {
